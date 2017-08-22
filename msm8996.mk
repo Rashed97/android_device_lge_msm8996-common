b70aa306bcd13098f4dbe1d27932644d313249c6
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:41:25 -0700

    msm8996: Add DRM HAL impl

--- msm8996.mk
+++ msm8996.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LGDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
