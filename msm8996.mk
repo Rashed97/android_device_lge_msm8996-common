31fc824883a2b6283083d6711da1fcdda80ab9cf
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-26 16:00:12 -0700

    msm8996: Switch to AOSP's pn5xx NFC stack

--- msm8996.mk
+++ msm8996.mk
@@ -261,13 +261,8 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    libnqnfc-nci \
-    libp61-jcop-kit \
-    nfc_nci.nqx.default \
-    NQNfcNci \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml \
+    nfc_nci.msm8996 \
+    NfcNci \
     Tag \
     android.hardware.nfc@1.0-impl
 
